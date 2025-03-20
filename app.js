a86d584c90bc4b5b9addc6985b79298ffe4d948d
Tanzeel Qamar <165699086+tanzeelqamar@users.noreply.github.com>
2025-03-21 01:36:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,96 @@
+//alert
+
+//Greeting
+alert("Hello, Welcome to this project")
+
+//error message
+alert("Error, Please enter a valid password")
+
+//using line break
+alert("Welcome to JS Land...\nHappy Coding!")
+
+//a message with checkbox
+alert("Happy Coding!\nPrevent this message from creating additional dialogs")
+
+// confirm("Prevent this message from creating additional dialogs")
+// prompt("Prevent this message from creating additional dialogs")
+
+//message through console
+let message1=("Hello... I can run JS through my web browser's console")
+console.log(message1);
+
+//variable string
+
+//Declare a variable called username.
+var username;
+
+//Declare a variable called myName & assign to it a string that represents your Full Name.
+var myName = "Tanzeel Qamar";
+console.log(myName);
+
+
+//Declare a JS variable, titled message.
+var message;
+
+//Assign “Hello World” to variable message
+message = "Hello World";
+console.log(message);
+
+
+//Display the message in alert box.
+alert(message)
+
+//save student’s bio data in JS variables and show the data in alert boxes.
+var name = "John Doe";
+var age = "15 years old";
+var education = "Certified Mobile Application Development";
+
+alert(name)
+alert(age)
+alert(education)
+
+//display the alert using one JS variable
+var a = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(a) 
+
+//variable called email and assign to it a string
+var email="abc@gmail.com"
+console.log(email);
+
+//Show message in an alert box.by using string concatenation
+alert(`My email address is ${email}`)
+
+//Declare a variable called book and display message
+var book="A smarter way to learn JavaScript"
+alert(`I am trying to learn from the Book ${book}`)
+
+//display in browser
+document.write("Yah! I can write HTML content through javascript");
+
+//store string in var and show in alert
+var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+alert(design)
+
+//variable numbers
+
+//age declaration
+var age = 23;
+alert(`I am ${age} years old`)
+
+//visit count
+var visitCount =1;
+alert(`you have visited this site ${visitCount} times`)
+var visitCount1 =2;
+alert(`you have visited this site ${visitCount1} times`)
+
+//birth year
+var birthYear = 2002
+document.write(`My birth year is ${birthYear}\n\n`) 
+
+//clothing store
+var name="John"
+var order=5
+var storeName="'xyz clothing store'"
+document.write(`${name} ordered ${order} T-shirts from ${storeName}`) 
+
